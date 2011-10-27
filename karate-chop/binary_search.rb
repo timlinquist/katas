afcee7a50f902dba716b2a35e04aab7ae798ba15
Tim Linquist <tim.linquist@gmail.com>
2011-10-26 22:55:11 -0700

    Initial start of recursive chop function
    Return -1 when 0 / 1 elements & none match target. Return target when 1
    elements and target matches. Next steps is to make function recursive.

--- karate-chop/binary_search.rb
+++ karate-chop/binary_search.rb
@@ -1,5 +1,24 @@
 class BinarySearch
-  def self.chop(position, collection)
+  def self.chop(target, collection)
+    #Return -1 if the collection is empty
+    return -1 if collection.empty? or (collection.size == 1 && collection.first != target)
+    return target if collection.size == 1 && collection.first == target
 
+    #Get the middle element, index of the current array
+    interval_index, interval_element = middle(collection)
+
+    return interval_index if interval_element == target
+    # chop(interval_index, collection)
+  end
+
+  def self.middle(collection=[])
+    last_elements_pos = collection.size
+    last_elements_pos += 1 if odd?(last_elements_pos)
+    middle_elements_pos = last_elements_pos / 2
+    return middle_elements_pos, collection.fetch(middle_elements_pos)
+  end
+
+  def self.odd? (number)
+    number % 2 == 1
   end
 end
